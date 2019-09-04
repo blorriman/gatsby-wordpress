7313e780db209eb89b6dd428696e794286966b1c
Robert Lorriman <robertlorriman@Roberts-MacBook-Pro.local>
2019-09-04 10:33:07 -0700

    fixed build error

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteMetadata: {
+    siteUrl: `https://bl-gatsby-wordpress.netlify.com/`,
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
