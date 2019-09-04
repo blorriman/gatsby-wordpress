f77b6f3b088ca0c6b79dd9431e2c7103f0ab1c45
Robert Lorriman <robertlorriman@Roberts-MacBook-Pro.local>
2019-09-04 14:33:13 -0700

    added blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,59 @@
+/* eslint-disable */
+import React, { useState } from "react"
+import PropTypes from "prop-types"
+import { Link, graphql } from "gatsby"
+import { Box, Container, Grid, makeStyles, Typography } from "@material-ui/core"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const useStyles = makeStyles(theme => ({
+  paragraph: {
+    ...theme.typography.body1,
+  },
+}))
+
+const Blog = ({ data }) => {
+  const classes = useStyles()
+
+  const posts = data.posts.edges
+
+  const displayPosts = () => {
+    return posts.map(post => {
+      return (
+        <li key={post.node.slug}>
+          <Link to={`/post/${post.node.slug}`}>{post.node.title}</Link>
+        </li>
+      )
+    })
+  }
+
+  return (
+    <Layout>
+      <Container component="main" className={classes.main} maxWidth="md">
+        <SEO title="Blog" />
+        <Typography component="div">
+          <Box fontSize="h4.fontSize" textAlign="center" m={1}>
+            Blog Page
+          </Box>
+          <ul>{displayPosts()}</ul>
+        </Typography>
+      </Container>
+    </Layout>
+  )
+}
+
+export default Blog
+
+export const query = graphql`
+  query {
+    posts: allWordpressPost {
+      edges {
+        node {
+          slug
+          title
+        }
+      }
+    }
+  }
+`
